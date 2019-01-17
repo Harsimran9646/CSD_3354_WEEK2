2669240c39955cacd1a58967f64f3ac78687d340
Harsimran9646 <46795232+Harsimran9646@users.noreply.github.com>
2019-01-17 16:56:42 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0707995
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+            dogs.Add("bulldog");
+            dogs.Add("collie");
+            dogs.Add("pitbull");
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog + " ");
+            }
+            Console.ReadLine();
+            int[] a1 = new int[] { 1, 3, 5, 7, 7, 9, 11 };
+            foreach (int i in a1)
+            {
+                Console.WriteLine(i + "");
+            }
+        } 
+
+    }
+}
